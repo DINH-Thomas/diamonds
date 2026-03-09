ba47a8e0d4eaf21f01dc3fc6e453c8660e04080d
Thomas Dinh <th_dinh@yahoo.fr>
2026-03-09 18:09:16 +0100

    Commit de fin de journée

--- src/diamonds/data.py
+++ src/diamonds/data.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer, make_column_selector
+from sklearn.impute import SimpleImputer, KNNImputer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 # Import other necessary libraries here
 
 
@@ -21,6 +26,10 @@ def load_data(cache = True) -> pd.DataFrame:
     return df_diamonds
     pass
 
+def keep_not_null(row) :
+    if 0 in row.values : return False
+    return True
+
 def clean_data(df: pd.DataFrame) -> pd.DataFrame:
     """
     Clean the diamonds dataset.
@@ -35,23 +44,9 @@ def clean_data(df: pd.DataFrame) -> pd.DataFrame:
     pd.DataFrame
         The cleaned diamonds dataset
     """
-    pass
-
-def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Preprocess the diamonds dataset.
-
-    Parameters
-    ----------
-    df : pd.DataFrame
-        The cleaned diamonds dataset
-
-    Returns
-    -------
-    pd.DataFrame
-        The preprocessed diamonds dataset
-    """
-    pass
+    df_diamonds = df.dropna()
+    df_clean = df_diamonds[df_diamonds.apply(keep_not_null,axis=1)]
+    return df_clean
 
 def create_X_y(df: pd.DataFrame) ->tuple[pd.DataFrame, pd.Series]:
     """
@@ -67,7 +62,64 @@ def create_X_y(df: pd.DataFrame) ->tuple[pd.DataFrame, pd.Series]:
     (pd.DataFrame, pd.Series)
         The feature matrix X and target vector y
     """
-    pass
+    X = df.drop(columns=["price"])
+    y = df["price"]
+    return X, y
+
+def split_data(X: pd.DataFrame, y: pd.Series) -> tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
+    """
+    Split the feature matrix X and target vector y into training and testing sets.
+
+    Parameters
+    ----------
+    X : pd.DataFrame
+        The feature matrix
+    y : pd.Series
+        The target vector
+
+    Returns
+    -------
+    (pd.DataFrame, pd.DataFrame, pd.Series, pd.Series)
+        The training and testing sets for X and y
+    """
+    X_train, X_test, y_train, y_test  = train_test_split(X,y, random_state=42)
+    return X_train, X_test, y_train, y_test
+
+
+def preprocess_data(df: pd.DataFrame, fit: bool = True) -> pd.DataFrame:
+    """
+    Preprocess the diamonds dataset.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The cleaned diamonds dataset
+
+    Returns
+    -------
+    pd.DataFrame
+        The preprocessed diamonds dataset
+    """
+    cat_pipe = Pipeline(
+    [  ("cat_imp",SimpleImputer(strategy="most_frequent"))
+      ,("ohe",OneHotEncoder(drop="first",sparse_output=False))
+        ])
+    num_pipe = Pipeline(
+    [("knn_imp", KNNImputer(n_neighbors=5))
+     ,("scaler", StandardScaler())
+      ])
+    preprocessor = ColumnTransformer(
+    [("numeric",num_pipe, make_column_selector(dtype_include="number"))
+    ,("categorical", cat_pipe, make_column_selector(dtype_exclude="number"))
+      ]).set_output(transform="pandas")
+    if fit :
+        df_preprocessed = preprocessor.fit_transform(df)
+    else :
+        df_preprocessed = preprocessor.transform(df)
+    return df_preprocessed
+    
+
+
 
 
 
--- src/diamonds/model.py
+++ src/diamonds/model.py
@@ -1,4 +1,16 @@
-from sklearn.base import BaseEstimator, Pipeline
+from sklearn.base import BaseEstimator
+import pandas as pd
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer, make_column_selector
+from sklearn.impute import SimpleImputer, KNNImputer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.linear_model import LinearRegression, ElasticNet
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.svm import SVR
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score, mean_absolute_percentage_error
 
 
 def create_model(model_name: str) -> BaseEstimator:
@@ -15,21 +27,28 @@ def create_model(model_name: str) -> BaseEstimator:
     BaseEstimator
         The model ready to be fitted
     """
-    pass
+    return RandomForestRegressor(n_estimators=500)
 
 def create_preproc() -> Pipeline:
     """
     Create a preprocessing pipeline.
     """
-    pass
+    cat_pipe = Pipeline(
+    [  ("cat_imp",SimpleImputer(strategy="most_frequent"))
+      ,("ohe",OneHotEncoder(drop="first",sparse_output=False))
+        ])
+    num_pipe = Pipeline(
+    [("knn_imp", KNNImputer(n_neighbors=5))
+     ,("scaler", StandardScaler())
+      ])
+    preprocessor = ColumnTransformer(
+    [("numeric",num_pipe, make_column_selector(dtype_include="number"))
+    ,("categorical", cat_pipe, make_column_selector(dtype_exclude="number"))
+      ]).set_output(transform="pandas")
+    return preprocessor
 
 def train_model(model, X_train, y_train):
-    pass
-
-def evaluate_model(model, X_test, y_test) -> dict[str, float]:
-    # NB : mae, mse, r2_score, mape
-    # Only print the metrics for now
-    pass
+    return model.fit(X_train, y_train)
 
 def predict(model, X):
     """
@@ -47,4 +66,17 @@ def predict(model, X):
     pd.Series
         The predicted values
     """
+    return model.predict(X)
+
+def evaluate_model(model, X_test, y_test) -> dict[str, float]:
+    # NB : mae, mse, r2_score, mape
+    # Only print the metrics for now 
+    y_pred = predict(model, X_test)
+    mae = mean_absolute_error(y_test,y_pred)
+    mse = mean_squared_error(y_test,y_pred)
+    r2  = r2_score(y_test,y_pred)
+    mape = mean_absolute_percentage_error(y_test,y_pred)
+    scores = {"mae":mae,"mse":mse,"r2":r2,"mape":mape}
+    print(scores)
+    return scores
     
